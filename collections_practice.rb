dde4d3f6e6098e0ff56bfa7888d968e88ec8e1f1
Paul <pcherian10@gmail.com>
2018-05-01 17:51:57 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,11 @@
+def sort_array_asc (array)
+  array.sort
+end
 
+def sort_array_desc (array)
+  array.sort.reverse
+end
+
+def sort_array_char_count (array)
+  array.min {|a, b| a.length <=> b.length}
+end
