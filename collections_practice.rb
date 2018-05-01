1cd706dc4a81b8f5e175080c157dae4d008d8500
Paul <pcherian10@gmail.com>
2018-05-01 18:14:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,6 +19,13 @@ def swap_elements (array)
   return array
 end
 
+def swap_elements_from_to (array, index, destination_index)
+
+end
+
+
+
+
 def reverse_array (array)
-  array.sort {|a, b| a.length <=> b.length}
+
 end
